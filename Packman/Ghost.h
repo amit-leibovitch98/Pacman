32fe8a773fe28cd31b4ec9ca914fe5bf25edfeff
amit-leibovitch98 <amitle111@gmail.com>
2021-12-03 23:14:41 +0200

    Ceature class added 3.12.21

--- Packman/Ghost.h
+++ Packman/Ghost.h
@@ -2,34 +2,20 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "Location.h"
+#include "Creature.h"
 
-class Ghost
+class Ghost: public Creature
 {
 private:
-
-	Location currLocation;
-	Location lastLocation;
-	const Location initLocation;
-	diraction currDiraction;
 	bool run_over_breadcramp;
-	const char character;
 
 public:
-	Ghost(Location _location, diraction _currDiraction, bool _run_over_breadcramp = true) :currLocation(_location), initLocation(_location), lastLocation(_location), currDiraction(_currDiraction), character('$'), run_over_breadcramp(_run_over_breadcramp) {};
+	Ghost(Location _location, bool _run_over_breadcramp = false):
+		Creature('$', _location), run_over_breadcramp(_run_over_breadcramp) {};
 	Ghost(const Ghost& other);
 	Ghost& operator=(const Ghost& other);
-	void setLocation(Location _location);
-	Location getLocation();
-	Location getLastLocation();
-	void setLastLocation(Location location);
-	Location getinitLocation() const;
 	void initGhostLocation(int i);
-	char getCharacter();
+	void ghostMoveDecider();
 	bool getRunOverBreadcramp();
 	void setRunOverBreadcramp(bool _run_over_breadcramp);
-	void setCurrDiraction(diraction _currDiraction);
-	diraction getCurrDiraction();
-	void ghostMoveDecider();
-
 };
